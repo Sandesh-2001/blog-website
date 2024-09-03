d69a817212829e1bc8a97c2f007756e54c7cab29
sandesh <sandeshshingare2001@gmail.com>
2024-09-03 19:35:21 +0530

    global error handler added.

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,13 +3,27 @@ const cors = require("cors");
 const blogModel = require("./models/blog.model");
 const validateBlog = require("./middlewares/joi");
 const blogValidations = require("./validations/blogs.validation");
+const CustomError = require("./utils/custom-error-handler");
+const errorHandler = require("./utils/error-handler");
+const asyncErrorHandler = require("./utils/async-error-handler");
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("", validateBlog(blogValidations.addBlog), async (req, res) => {
-  let blog = await blogModel.create({ title: "hello" });
-  res.status(200).json(blog);
-});
+app.get(
+  "",
+  validateBlog(blogValidations.addBlog),
+  asyncErrorHandler(async (req, res, next) => {
+    throw new Error("error of throw");
+    next(error);
+    let blog = await blogModel.create({ title: "hello" });
+    res.status(200).json(blog);
+  })
+);
 
+app.all("*", (req, res, next) => {
+  const error = new CustomError(`The URL ${req.originalUrl} not found`);
+  next(error);
+});
+app.use(errorHandler);
 module.exports = app;
