fd025ae7c6a9bcab79281766df604a7e54852896
sandesh-2001 <sandeshshingare2001@gmail.com>
2024-09-16 12:22:29 +0530

    pipe code fixed.

--- frontend/src/app/core/pipes/time-till-now.pipe.ts
+++ frontend/src/app/core/pipes/time-till-now.pipe.ts
@@ -10,86 +10,74 @@ export class TimeTillNowPipe implements PipeTransform {
     // console.log('from time till now pipe...', value);
     value = String(value);
     // console.log('args', args);
-    let date1Seconds: any = new Date('' + value).getUTCSeconds();
-    let date1Minutes: any = new Date('' + value).getUTCMinutes();
-    let date1Hours: any = new Date('' + value).getUTCHours();
-    let date1Day = new Date('' + value).getUTCDay();
-    let date1Months = new Date('' + value).getUTCMonth();
-    let date1Years = new Date('' + value).getUTCFullYear();
-    // console.log('full year ', date1Years);
 
-    let date2Seconds: any = new Date(Date.now()).getUTCSeconds();
-    let date2Minutes: any = new Date(Date.now()).getUTCMinutes();
-    let date2Hours: any = new Date(Date.now()).getUTCHours();
-    let date2Day = new Date(Date.now()).getUTCDay();
-    let date2Months = new Date(Date.now()).getUTCMonth();
-    let date2Years = new Date(Date.now()).getUTCFullYear();
-
-    var differenceSeconds = date1Seconds - date2Seconds;
-    var differenceHours = date1Hours - date2Hours;
-    var differenceMinutes = date1Minutes - date2Minutes;
-    var differenceDays = date1Day - date2Day;
-    var differenceMonths = date1Months - date2Months;
-    var differenceYears = date1Years - date2Years;
-
-    var defaultValue: any = new Date('' + value).toLocaleDateString();
-
-    const result = this.getResult(
-      differenceSeconds,
-      differenceMinutes,
-      differenceHours,
-      differenceDays,
-      differenceMonths,
-      differenceYears,
-      defaultValue
-    );
+    const result = this.getResult(value);
 
     // console.log('difference is', difference);
     // let time = new Date().getTime() - value.getTime();
     return result + ' ago';
   }
 
-  getResult(
-    seconds: number,
-    minutes: number,
-    hours: number,
-    days: number,
-    months: number,
-    years: number,
-    defaultValue: any
-  ): string {
-    seconds = Math.abs(seconds);
-    minutes = Math.abs(minutes);
-    hours = Math.abs(hours);
-    days = Math.abs(days);
-    months = Math.abs(months);
-    years = Math.abs(years);
-    // console.log('seconds===>>', seconds);
-    // console.log('minutes===>>', minutes);
-    // console.log('hours===>>', hours);
-    // console.log('day===>>', days);
-    // console.log('months===>>', months);
-    // console.log('years===>>', years);
+  getResult(from: any, till: any = Date.now()): string {
+    var defaultValue: any = new Date('' + from).toLocaleDateString();
 
-    // console.log('__________________________________________________________');
+    let date1Seconds: any = new Date('' + from).getUTCSeconds();
+    let date1Minutes: any = new Date('' + from).getUTCMinutes();
+    let date1Hours: any = new Date('' + from).getUTCHours();
+    let date1Date = new Date('' + from).getUTCDate();
+    let date1Months = new Date('' + from).getUTCMonth();
+    let date1Years = new Date('' + from).getUTCFullYear();
+
+    let date2Seconds: any = new Date(till).getUTCSeconds();
+    let date2Minutes: any = new Date(till).getUTCMinutes();
+    let date2Hours: any = new Date(till).getUTCHours();
+    let date2Date = new Date(till).getUTCDate();
+    let date2Months = new Date(till).getUTCMonth();
+    let date2Years = new Date(till).getUTCFullYear();
+
+    var differenceSeconds = Math.abs(date1Seconds - date2Seconds);
+    var differenceHours = Math.abs(date1Hours - date2Hours);
+    var differenceMinutes = Math.abs(date1Minutes - date2Minutes);
+    var differenceDate = Math.abs(date1Date - date2Date);
+    var differenceMonths = Math.abs(date1Months - date2Months);
+    var differenceYears = Math.abs(date1Years - date2Years);
+
     if (
-      seconds < 60 &&
-      minutes <= 0 &&
-      hours <= 0 &&
-      days <= 0 &&
-      months <= 0
+      differenceSeconds < 60 &&
+      differenceMinutes <= 0 &&
+      differenceHours <= 0 &&
+      differenceDate <= 0 &&
+      differenceMonths <= 0
     ) {
-      return seconds < 9 ? '0' + seconds + ' sec.' : seconds + ' sec';
-    } else if (minutes < 60 && hours <= 0 && days <= 0) {
-      return minutes + ' min, ' + seconds + ' sec.';
-    } else if (hours <= 24 && days <= 0 && months <= 0) {
-      return hours <= 1 ? hours + 'hour' : hours + ' hours';
-    } else if (days <= 7 && months <= 0) {
-      return days <= 1 ? days + ' day' : days + ' days';
-    } else if (months <= 12 && years <= 0) {
-      return months <= 1 ? months + ' month' : months + ' months';
+      return differenceSeconds < 9
+        ? '0' + differenceSeconds + ' sec'
+        : differenceSeconds + ' sec';
+    } else if (
+      differenceMinutes < 60 &&
+      differenceHours <= 0 &&
+      differenceDate <= 0
+    ) {
+      return differenceMinutes + ' min, ' + differenceSeconds + ' sec.';
+    } else if (
+      differenceHours <= 24 &&
+      differenceDate <= 0 &&
+      differenceMonths <= 0
+    ) {
+      return differenceHours <= 1
+        ? differenceHours + 'hour'
+        : differenceHours + ' hours';
+    } else if (differenceDate <= 31 && differenceMonths <= 0) {
+      return differenceDate <= 1
+        ? differenceDate + ' day'
+        : differenceDate + ' days';
+    } else if (differenceMonths <= 12 && differenceYears <= 0) {
+      return differenceMonths <= 1
+        ? differenceMonths + ' month'
+        : differenceMonths + ' months';
     } else {
-      return defaultValue;
+      return differenceYears <= 1
+        ? differenceYears + 'year'
+        : differenceYears + 'years';
     }
   }
 }
